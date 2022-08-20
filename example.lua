70cb842fcff86443d930fcbdee29d8e6bef736ec
Sergey Bronnikov <sergeyb@tarantool.org>
2022-08-20 10:15:28 +0300

    example

---
+++ example.lua
@@ -0,0 +1,31 @@
+local luzer = require("luzer")
+local fdp = luzer.FuzzedDataProvider(input_bytes)
+local math = require("math")
+local lib = luzer.require_instrument("lib")
+
+local function custom_mutator(data, max_size, seed)
+    return data .. "xxx"
+end
+
+local function TestOneInput(data)
+  --[[
+  The entry point for our fuzzer.
+
+  This is a callback that will be repeatedly invoked with different arguments
+  after Fuzz() is called.
+  We translate the arbitrary byte string into a format our function being fuzzed
+  can understand, then call it.
+
+  Args:
+    data: Bytestring coming from the fuzzing engine.
+  ]]
+
+  return
+end
+
+if #arg > 1 and arg[1] == "--no_mutator" then
+    luzer.Setup(argv, TestOneInput, custom_mutator)
+else
+    luzer.Setup(argv, TestOneInput)
+
+luzer.Fuzz()
