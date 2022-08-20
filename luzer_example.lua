b626283ab3da7c18bf1ef609e293f03eb83fbb61
Sergey Bronnikov <sergeyb@tarantool.org>
2022-08-20 11:25:33 +0300

    fixes

--- luzer_example.lua
+++ luzer_example.lua
@@ -8,24 +8,26 @@ local function custom_mutator(data, max_size, seed)
 end
 
 local function TestOneInput(data)
-  -- The entry point for our fuzzer.
-  --
-  -- This is a callback that will be repeatedly invoked with different
-  -- arguments after Fuzz() is called.
-  --
-  -- We translate the arbitrary byte string into a format our function being
-  -- fuzzed can understand, then call it.
-  --
-  -- Args:
-  --    data: string coming from the fuzzing engine.
+    -- The entry point for our fuzzer.
+    --
+    -- This is a callback that will be repeatedly invoked with different
+    -- arguments after Fuzz() is called.
+    --
+    -- We translate the arbitrary byte string into a format our function being
+    -- fuzzed can understand, then call it.
+    --
+    -- Args:
+    --    data: string coming from the fuzzing engine.
+    local fdp = luzer.FuzzedDataProvider(10)
+    print(data .. fdp.consume_string())
 
-  return
+    return
 end
 
 if #arg > 1 and arg[1] == "--no_mutator" then
-    luzer.Setup(argv, TestOneInput, custom_mutator)
+    luzer.Setup(arg, TestOneInput, custom_mutator)
 else
-    luzer.Setup(argv, TestOneInput)
+    luzer.Setup(arg, TestOneInput)
 end
 
 luzer.Fuzz()
