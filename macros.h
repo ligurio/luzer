6126850d11b1d1c997d1dfafbd0367f26bfde439
Sergey Bronnikov <sergeyb@tarantool.org>
2022-08-24 22:42:06 +0300

    macros

---
+++ macros.h
@@ -0,0 +1,31 @@
+#ifndef LUZER_MACROS_H_
+#define LUZER_MACROS_H_
+
+#include <assert.h>
+
+/**
+ * If control flow reaches the point of the unreachable(), the program is
+ * undefined. It is useful in situations where the compiler cannot deduce
+ * the unreachability of the code.
+ */
+#if __has_builtin(__builtin_unreachable) || defined(__GNUC__)
+#  define unreachable() (assert(0), __builtin_unreachable())
+#else
+#  define unreachable() (assert(0))
+#endif
+
+#define NO_SANITIZE_ADDRESS __attribute__((no_sanitize_address))
+
+#ifdef __has_attribute
+#if __has_attribute(no_sanitize)
+#define NO_SANITIZE_MEMORY __attribute__((no_sanitize("memory")))
+#else
+#define NO_SANITIZE_MEMORY
+#endif  // __has_attribute(no_sanitize)
+#else
+#define NO_SANITIZE_MEMORY
+#endif  // __has_attribute
+
+#define NO_SANITIZE NO_SANITIZE_ADDRESS NO_SANITIZE_MEMORY
+
+#endif  // LUZER_MACROS_H_
