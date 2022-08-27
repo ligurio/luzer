4694f6155768541b905e96d56776e8d7194acd63
Sergey Bronnikov <sergeyb@tarantool.org>
2022-08-27 21:12:00 +0300

    fix tests

--- test.lua
+++ test.lua
@@ -70,25 +70,6 @@ assert(type(res) == "table")
 assert(res[1] == 230)
 assert(res[2] == 430)
 
---[[
-assert(type(fdp.consume_cdata) == "function")
-res = fdp.consume_cdata()
-assert(res == nil)
-
-assert(type(fdp.consume_userdata) == "function")
-res = fdp.consume_userdata()
-assert(res == nil)
-
-assert(type(fdp.consume_lightuserdata) == "function")
-res = fdp.consume_lightuserdata()
-assert(res == nil)
-]]
-
-assert(type(fdp.consume_remaining_as_string) == "function")
-res = fdp.consume_remaining_as_string()
-assert(type(res) == "string")
-assert(res == "remaining")
-
 assert(type(fdp.remaining_bytes) == "function")
 res = fdp.remaining_bytes()
 assert(type(res) == "number")
