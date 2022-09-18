42987412dc2f98cb75548fdcf4e64c87db0a7606
Sergey Bronnikov <sergeyb@tarantool.org>
2022-09-18 22:31:38 +0300

    string stdlib

--- examples/libc.lua
+++ examples/libc.lua
@@ -2,11 +2,14 @@
 --
 -- https://github.com/squeek502/zig-std-lib-fuzzing
 -- https://sourceware.org/glibc/wiki/FuzzingLibc
+-- https://github.com/novafacing/libc-fuzzer
 -- LLVM libc-fuzzer https://gitlab.itwm.fraunhofer.de/kai_plociennik/spu-llvm/-/tree/master/libc/fuzzing
 -- https://forallsecure.com/blog/cve-2020-10029-buffer-overflow-in-gnu-libc-trigonometry-functions
 -- https://android.googlesource.com/toolchain/llvm-project/+/refs/heads/android12-release/libc/fuzzing/
 -- qsort
+-- https://github.com/codenote/regfuzz
 -- regex
+-- https://github.com/novafacing/libmusl
 
 local luzer = require("luzer")
 local has_ffi, ffi = pcall(require, "ffi")
--- examples/stdlib_string.lua
+++ examples/stdlib_string.lua
@@ -1,48 +1,116 @@
 -- https://github.com/Zac-HD/stdlib-property-tests
 -- https://github.com/Zac-HD/stdlib-property-tests/blob/master/tests/test_re_by_construction.py
 -- Differential testing with re, https://luarocks.org/modules/rrt/lrexlib-posix
+-- https://github.com/codenote/regfuzz
+
+-- 20.2 – Patterns
+-- https://www.lua.org/pil/20.2.html
+
+-- Source code: http://www.lua.org/source/5.1/lstrlib.c.html
+
+-- s = "Deadline is 30/05/1999, firm"
+-- date = "%d%d/%d%d/%d%d%d%d"
+-- print(string.sub(s, string.find(s, date)))   --> 30/05/1999
+
+local luzer = require("luzer")
+
+local function TestOneInput_find(buf, _size)
+    local fdp = luzer.FuzzedDataProvider(buf)
+    local str = fdp:consume_string(5)
+    local pattern  = fdp:consume_string(5)
+    string.find(str, pattern)
+end
+
+local function TestOneInput_gsub(buf)
+    local fdp = luzer.FuzzedDataProvider(buf)
+    local str = fdp:consume_string(5)
+    local pattern  = fdp:consume_string(5)
+    local rep = fdp:consume_string(5)
+    string.gsub(str, pattern, rep)
+end
+
+local function TestOneInput_sub(buf)
+    local fdp = luzer.FuzzedDataProvider(buf)
+    local str = fdp:consume_string(100)
+    local pattern  = fdp:consume_string(100)
+    string.sub(str, string.find(str, pattern))
+end
+
+local function TestOneInput_lower(buf)
+    local fdp = luzer.FuzzedDataProvider(buf)
+    local str = fdp:consume_string(100)
+    string.lower(str)
+end
+
+local function TestOneInput_upper(buf)
+    local fdp = luzer.FuzzedDataProvider(buf)
+    local str = fdp:consume_string(100)
+    string.upper(str)
+end
+
+local function TestOneInput_match(buf)
+    local fdp = luzer.FuzzedDataProvider(buf)
+    local str = fdp:consume_string(100)
+    string.match(str)
+end
+
+local function TestOneInput_gmatch(buf)
+    local fdp = luzer.FuzzedDataProvider(buf)
+    local str = fdp:consume_string(100)
+    string.gmatch(str)
+end
+
+local function TestOneInput_reverse(buf)
+    local fdp = luzer.FuzzedDataProvider(buf)
+    local str = fdp:consume_string(100)
+    assert(string.reverse(string.reverse(str)) == str)
+end
+
+local function TestOneInput_format(buf)
+    local fdp = luzer.FuzzedDataProvider(buf)
+    local str = fdp:consume_string(100)
+    local args = fdp:consume_strings(100)
+    --string.format(str, table.unpack(args))
+end
+
+local function TestOneInput_len(buf)
+    local fdp = luzer.FuzzedDataProvider(buf)
+    local len = fdp:consume_number()
+    local str = fdp:consume_string(len)
+    assert(#str == len)
+    assert(string.len(str) == len)
+end
+
+local function TestOneInput_byte(buf)
+    local fdp = luzer.FuzzedDataProvider(buf)
+    local b = fdp:consume_string(1)
+    local char_code = string.byte(b)
+    assert(type(char_code) == "number")
+    local byte = string.char(char_code)
+    assert(byte == b)
+end
 
 --[[
--- Lua 5.1
-"string.find"
-"string.format"
 "string.rep"
-"string.gsub"
-"string.len"
-"string.gmatch"
 "string.dump"
-"string.match"
-"string.reverse"
-"string.byte"
-"string.char"
-"string.upper"
-"string.lower"
-"string.sub"
+]]
 
+local arg1 = {"-max_len=4096", "-only_ascii=1", "-dict=/home/sergeyb/sources/luzer/dict_example"}
+
+luzer.Setup(arg1, TestOneInput)
+luzer.Fuzz()
+
+--[[
 -- Tarantool
-"string.find"
 "string.ljust"
-"string.len"
-"string.gsub"
-"string.upper"
-"string.sub"
-"string.char"
 "string.rstrip"
-"string.match"
 "string.split"
 "string.center"
 "string.strip"
 "string.endswith"
-"string.rep"
 "string.hex"
-"string.lower"
 "string.fromhex"
-"string.dump"
-"string.gmatch"
-"string.reverse"
-"string.byte"
 "string.startswith"
 "string.rjust"
 "string.lstrip"
-"string.format"
 ]]
