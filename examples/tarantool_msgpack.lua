0c71a0db62c18bdd0d2f9d8db5666e72ca26874f
Sergey Bronnikov <estetus@gmail.com>
2022-10-15 22:39:19 +0300

    corpus

--- examples/tarantool_msgpack.lua
+++ examples/tarantool_msgpack.lua
@@ -1,11 +1,11 @@
 --[[
-https://github.com/tarantool/tarantool/issues/206
 https://github.com/tarantool/tarantool/issues/5184
-https://github.com/tarantool/tarantool/issues/5017
-https://github.com/tarantool/tarantool/issues/5016
-https://github.com/tarantool/tarantool/issues/5014
 https://github.com/tarantool/tarantool/issues/4724
 https://github.com/tarantool/tarantool/issues/3900
+https://github.com/tarantool/tarantool/issues/5014 "\xd4\x02\x00"
+https://github.com/tarantool/tarantool/issues/5016 "\xd4\xfe\x00"
+https://github.com/tarantool/tarantool/issues/5017 "\xd4\x0f\x00"
+https://github.com/tarantool/tarantool/issues/206
 
 https://www.tarantool.io/ru/doc/latest/reference/reference_lua/msgpack/
 ]]
@@ -19,7 +19,7 @@ local function TestOneInput(buf)
         ok, res = pcall(msgpack.encode, res)
         if ok == false and
            string.find(res, "Too high nest level") then
-            return
+            return -1
         end
         assert(ok == true)
         assert(res ~= nil)
@@ -34,5 +34,7 @@ end
 
 local args = {
     max_len = 4096,
+    dict = "/home/sergeyb/sources/luzer/examples/tarantool_msgpack.dict",
+    corpus = "/home/sergeyb/sources/luzer/examples/tarantool_msgpack",
 }
 luzer.Fuzz(TestOneInput, nil, args)
