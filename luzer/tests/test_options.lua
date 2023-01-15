2c72bd234800d46c6bce779483e21067d7d0aa98
Sergey Bronnikov <estetus@gmail.com>
2023-01-15 22:16:08 +0300

    New test with edge case

---
+++ luzer/tests/test_options.lua
@@ -0,0 +1,12 @@
+package.cpath = "./?.so"
+
+local luzer = require("luzer")
+
+local args = {
+    max_len = 1024,
+    print_pcs = 1,
+    corpus = "undefined",
+    max_total_time = 60,
+    print_final_stats = 1,
+}
+luzer.Fuzz(function() end, nil, args)
