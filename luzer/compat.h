6e3276c5e6faf18c8c98637162bea0a23f0cd1d3
Sergey Bronnikov <estetus@gmail.com>
2025-01-19 12:03:21 +0300

    luzer: fix building with LuaJIT library
    Follows up commit 069045d59af7
    ("luzer: fix stack overflow due to recursive traceback").

--- luzer/compat.h
+++ luzer/compat.h
@@ -1,7 +1,11 @@
 #ifndef LUZER_COMPAT_H_
 #define LUZER_COMPAT_H_
 
+#if !defined(LUA_VERSION_NUM) || (LUA_VERSION_NUM == 501 && !defined(IS_LUAJIT))
+
 void luaL_traceback(lua_State *L, lua_State *L1,
 					const char *msg, int level);
+
+#endif
 
 #endif  // LUZER_COMPAT_H_
