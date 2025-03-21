df2e5c4a3a0f225da85a169a23d6f708594a1a04
Sergey Bronnikov <estetus@gmail.com>
2025-03-21 15:22:22 +0300

    luzer: introduce a test with LuaJIT FFI
    The patch adds a test that tests a target function implemented
    in C and called using LuaJIT FFI. The current version of the test
    does not check anything, because luzer can't pass condition and
    reach `abort()`.
    
    Needed for the following commit.

---
+++ luzer/tests/test_ffi.lua
@@ -0,0 +1,21 @@
+local luzer = require("luzer")
+local has_ffi, ffi = pcall(require, "ffi")
+if not has_ffi then
+    io.stderr:write("FFI is not supported\n")
+    os.exit(0)
+end
+
+ffi.cdef[[
+int say_hello(const char *buf, size_t len);
+]]
+
+local testlib = ffi.load("testlib.so")
+
+local function TestOneInput(buf)
+    local fdp = luzer.FuzzedDataProvider(buf)
+    local max_len = fdp:consume_integer(1, 100)
+    local str = fdp:consume_string(max_len)
+    testlib.say_hello(str, #str + 1)
+end
+
+luzer.Fuzz(TestOneInput, nil, {})
