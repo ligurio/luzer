ebab207eadd085768f933412666a70088e51d093
Sergey Bronnikov <estetus@gmail.com>
2025-03-28 18:19:26 +0300

    luzer: introduce luzer.path
    The patch introduces a method `path()` that returns a path to
    shared libraries with libFuzzer and sanitizers.
    `require('luzer').path` returns a table with paths to shared
    libraries with sanitizers:
    
    ```
    > luzer.path
    ---
    - ubsan: /home/sergeyb/sources/luzer/build/luzer/ubsan_with_fuzzer.so
      asan: /home/sergeyb/sources/luzer/build/luzer/asan_with_fuzzer.so
    ...
    ```
    
    Usage:
    
    LD_PRELOAD=$(luajit -e "print(require('luzer').path.asan)") \
      luajit examples/example_basic.lua

--- luzer/config.c
+++ luzer/config.c
@@ -9,3 +9,13 @@ NO_SANITIZE const char *
 luzer_version_string(void) {
 	return "@CMAKE_PROJECT_VERSION@";
 }
+
+NO_SANITIZE const char *
+dso_asan_string(void) {
+	return "@ASAN_DSO@";
+}
+
+NO_SANITIZE const char *
+dso_ubsan_string(void) {
+	return "@UBSAN_DSO@";
+}
