309e0ff7bc322de8669137cdfe6a0dd8721910c0
Sergey Bronnikov <estetus@gmail.com>
2026-07-16 19:39:59 +0300

    luzer: fix always truthy assertion

--- luzer/tests/test_luajit_friendly.lua
+++ luzer/tests/test_luajit_friendly.lua
@@ -14,7 +14,7 @@ local function TestOneInput(buf)
     local numbers = fdp:consume_numbers(0, 2*10^6, 10)
     for _, n in ipairs(numbers) do
         if n == 100500 then
-            assert("Bingo!")
+            error("Bingo!")
         end
     end
 
